255f977b55793b08f38d52a8478d38bc517ceb0f
Rowan Nicholas <78248776+rowannicholas1@users.noreply.github.com>
2023-03-13 10:05:16 -0700

    shit

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ import random as rand
 go_first = rand.randint(0, 1)
 if go_first == 0:
     print("computer will go first")
+    player_move = 0
+    computer_move = 1
 if go_first == 1:
     print("player will fo first")
+    player_move = 1
+    computer_move = 0
 
 x_o = rand.randint(0, 1)
 if x_o == 0:
     print("player will be x, computer will be o")
+    player_letter = X
+    computer_letter = 0
 if x_o == 1:
-    print("player will be x, computer will be o")
+    print("player will be o, computer will be x")
 
 wn = trtl.Screen()
 wn.tracer(False)
