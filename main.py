75164bcd8ee39722626e11a9cb253b0dbad866c4
Rowan Nicholas <78248776+rowannicholas1@users.noreply.github.com>
2023-03-14 08:55:46 -0700

    pencil

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''main.py'''
 import turtle as trtl
 import random as rand
 
@@ -5,18 +6,18 @@ import random as rand
 go_first = rand.randint(0, 1)
 if go_first == 0:
     print("computer will go first")
-    player_move = 0
-    computer_move = 1
+    PLAYER_MOVE = 0
+    COMPUTER_MOVE = 1
 if go_first == 1:
     print("player will fo first")
-    player_move = 1
-    computer_move = 0
+    PLAYER_MOVE = 1
+    COMPUTER_MOVE = 0
 
 x_o = rand.randint(0, 1)
 if x_o == 0:
     print("player will be x, computer will be o")
-    player_letter = X
-    computer_letter = 0
+    PLAYER_LETTER = 1  # X
+    COMPUTER_LETTER = 0  # O
 if x_o == 1:
     print("player will be o, computer will be x")
 
@@ -29,25 +30,29 @@ board.pensize(5)
 board.setheading(90)
 board.penup()
 
-board_width = 65
-board_length = 200
+BOARD_WIDTH = 65
+BOARD_LENGTH = 200
 
 
 def board_move():
+    '''removes repetitive code'''
     board.pendown()
     board.forward(400)
     board.penup()
 
 
-board.goto(-board_width, -board_length)
+board.goto(-BOARD_WIDTH, -BOARD_LENGTH)
 board_move()
-board.goto(board_width, -board_length)
+board.goto(BOARD_WIDTH, -BOARD_LENGTH)
 board_move()
 board.setheading(0)
-board.goto(-board_length, -board_width)
+board.goto(-BOARD_LENGTH, -BOARD_WIDTH)
 board_move()
-board.goto(-board_length, board_width)
+board.goto(-BOARD_LENGTH, BOARD_WIDTH)
 board_move()
+
+if go_first == 1:
+    print("select a box using your number keys")
 
 
 wn.mainloop()
