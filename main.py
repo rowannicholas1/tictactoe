e29d27175fa69d55f7aa8aa731a59c312bacfee3
Rowan Nicholas <78248776+rowannicholas1@users.noreply.github.com>
2023-03-14 09:56:24 -0700

    numberrrrsss

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ import random as rand
 go_first = rand.randint(0, 1)
 if go_first == 0:
     print("computer will go first")
-    PLAYER_MOVE = 0
-    COMPUTER_MOVE = 1
+    PLAYER_TURN = 0  # not players turn
 if go_first == 1:
     print("player will fo first")
-    PLAYER_MOVE = 1
-    COMPUTER_MOVE = 0
+    PLAYER_TURN = 1  # players turn
 
 x_o = rand.randint(0, 1)
 if x_o == 0:
@@ -60,12 +58,10 @@ board_move()
 board.goto(-BOARD_LENGTH, BOARD_WIDTH)
 board_move()
 
-if go_first == 1:
-    print("select a box using your number keys")
-
 PENCIL_IMAGE = "~/Documents/github/tictactoe/pencil.gif"
 wn.addshape(PENCIL_IMAGE)
 pencil = trtl.Turtle(shape=PENCIL_IMAGE)
+pencil.speed(10)
 pencil.penup()
 pencil.ht()
 pencil.shape(PENCIL_IMAGE)
@@ -73,6 +69,16 @@ pencil_default = (300, 0)
 pencil.goto(pencil_default)
 pencil.st()
 wn.update()
+
+# maybe change to for loop because there is only 9 moves total
+if PLAYER_TURN == 1:
+    print("it is your turn. select a box using your number keys")
+    # player does the things to make their turn
+    PLAYER_TURN == 0
+elif PLAYER_TURN == 0:
+    print("it is the computers turn")
+    # computer makes their turn
+    PLAYER_TURN == 1
 
 
 def draw_x():
@@ -84,11 +90,11 @@ def draw_o():
     '''draws an o'''
     pencil.circle(20)
 
+# TODO: add method to prevent player from spamming buttons to draw many shapes
 
-def key_1():
-    '''exectues a draw on key press'''
-    wn.tracer(True)
-    pencil.goto(C1, R1)
+
+def keypress():
+    '''executes drawing on key press'''
     pencil.pendown()
     if PLAYER_LETTER == 1:
         draw_x()
@@ -100,7 +106,78 @@ def key_1():
     wn.tracer(False)
 
 
-wn.onkeypress(key_1, "1")
+def key_1():
+    '''sends pencil to coordinate'''
+    wn.tracer(True)
+    pencil.goto(C1, R1)
+    keypress()
 
-wn.listen()
+
+def key_2():
+    '''sends pencil to coordinate'''
+    wn.tracer(True)
+    pencil.goto(C2, R1)
+    keypress()
+
+
+def key_3():
+    '''sends pencil to coordinate'''
+    wn.tracer(True)
+    pencil.goto(C3, R1)
+    keypress()
+
+
+def key_4():
+    '''sends pencil to coordinate'''
+    wn.tracer(True)
+    pencil.goto(C1, R2)
+    keypress()
+
+
+def key_5():
+    '''sends pencil to coordinate'''
+    wn.tracer(True)
+    pencil.goto(C2, R2)
+    keypress()
+
+
+def key_6():
+    '''sends pencil to coordinate'''
+    wn.tracer(True)
+    pencil.goto(C3, R2)
+    keypress()
+
+
+def key_7():
+    '''sends pencil to coordinate'''
+    wn.tracer(True)
+    pencil.goto(C1, R3)
+    keypress()
+
+
+def key_8():
+    '''sends pencil to coordinate'''
+    wn.tracer(True)
+    pencil.goto(C2, R3)
+    keypress()
+
+
+def key_9():
+    '''sends pencil to coordinate'''
+    wn.tracer(True)
+    pencil.goto(C3, R3)
+    keypress()
+
+
+wn.onkeypress(key_1, "1")
+wn.onkeypress(key_2, "2")
+wn.onkeypress(key_3, "3")
+wn.onkeypress(key_4, "4")
+wn.onkeypress(key_5, "5")
+wn.onkeypress(key_6, "6")
+wn.onkeypress(key_7, "7")
+wn.onkeypress(key_8, "8")
+wn.onkeypress(key_9, "9")
+
+wn.listen(True)
 wn.mainloop()
